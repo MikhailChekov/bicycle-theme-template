96140b7cf4ddc95c11200429276c759053509ba7
Mikhail Chekov <chekoff@list.ru>
2020-10-01 20:10:33 +0300

    finished 'slider', started 'shop'

--- src/js/script.js
+++ src/js/script.js
@@ -34,41 +34,68 @@
         menuBody.classList.toggle("active");
     });
 
-    const elems = '[id*=slider_page]',
-        btnParent = '.slider__buttons',
-        btnClass = 'slider__btn';
-
+    /**
+     * Usage: simpleSlider(elems, btnParetn[,btnClass])
+     * elems - string, define container with elems;
+     * btnParent - string, define btn container (should be 'ul'), where generate btns 
+     * with className - btnClass;
+    */
     const simpleSlider = (elems, btnParent, btnClass) => {
         elems = document.querySelectorAll(elems),
         btnParent = document.querySelector(btnParent),
-        btnClass = btnClass || 'btn';
+        btnClass = btnClass || 'slider_btn';
+        let position = 0;
 
-        let ul = document.createElement('ul');
+        // hide extra elems
+        for(let i = 0; i < elems.length; i++){
+            if(i !== position){
+                elems[i].style.display = "none";
+            }
+        }
+        
+        //create Btn's + create id to elems and connect with btn's.
         elems.forEach((el, i) => {
             let li = document.createElement('li');
             let btn = document.createElement('button');
             btn.className = btnClass;
-            btn.setAttribute('id', `btn_${el.attributes.id.value}`);
+            btn.setAttribute('id', `slider_btn_${i}`);
             btn.addEventListener('click', btnClickHandle);
+            el.setAttribute('id', `slider_element_${i}`);   
+            el.addEventListener('click', elemClickHandle);
+            if(i === position){ btn.classList.add('active');}
             li.append(btn);
-            ul.append(li);
+            btnParent.append(li);
         });
+        
 
-        btnParent.append(ul);
-
-        function btnClickHandle (e) {
-            let target = e.target;
-            if(target.classList.contains('acitve')) return;
-
-            target.classList.add('active');
-            elems.forEach(el => {
-                if(el !== target)
-                    el.classList.remove('active');
-            });
+        function showElem (pos, prevPos) {
+            elems[pos].style.display = "block";
+            btnParent.children[pos].children[0].classList.add('active');
+            if(pos !== prevPos){
+                elems[prevPos].style.display = "none";
+                btnParent.children[prevPos].children[0].classList.remove('active');
+            }
         }
 
-        
+        function elemClickHandle (e) {
+            let prevPos = position;
+            let posNumber = parseInt(e.currentTarget.id.match(/\d+/));
+            ((++posNumber) > elems.length - 1) ? 
+            position = 0 :
+            position = posNumber;
+            showElem(position, prevPos);
+        }
+
+        function btnClickHandle (e) {
+            let prevPos = position;
+            position = parseInt(e.target.id.match(/\d+/));
+            showElem(position, prevPos);
+        }
     }
+
+    const elems = '.slider__item',
+    btnParent = '.slider__buttons',
+    btnClass = 'slider__btn';
 
     simpleSlider(elems, btnParent, btnClass);
 
